2cc00ba67de3f02fef82e971eeb5d554ad71e47a
Sanford Leach <sanfordleach@gmail.com>
2025-01-23 00:42:47 -0500

    Refactor LoginCard and Login page layout: - Removed max-width and padding from #root in App.css for full-width layout. - Enhanced LoginCard component with new TextField inputs for email and password. - Updated Login page to include a pug image and adjusted layout using Flex for better responsiveness.

--- src/components/LoginCard.tsx
+++ src/components/LoginCard.tsx
@@ -1,18 +1,33 @@
-import { Box, Card, Flex, Heading } from "@radix-ui/themes";
+import { Box, Card, Flex, Heading, TextField } from "@radix-ui/themes";
 import styled from "styled-components";
 
 const StyledCard = styled(Card)`
   background-color: #f7f3ea;
+  padding: 2rem;
 `;
 
 export const LoginCard = () => {
   return (
-    <Box maxWidth="400px" mx="auto" p="4">
+    <Box width="500px" mx="auto" p="4">
       <StyledCard>
-        <Flex direction="row" gap="2" justify="center" align="center">
-          <Heading className="atma-medium font" size={"8"}>
-            Login
-          </Heading>
+        <Flex direction="column" gap="4">
+          <Flex width="100%" justify="center" mb="2">
+            <Heading className="atma-medium font" size={"8"}>
+              Login
+            </Heading>
+          </Flex>
+          <Flex direction="column">
+            <Heading className="font" size={"3"}>
+              Email address
+            </Heading>
+            <TextField.Root placeholder="email@example.com" type="email" />
+          </Flex>
+          <Flex direction="column">
+            <Heading className="font" size={"3"}>
+              Password
+            </Heading>
+            <TextField.Root placeholder="YourDogsName1234" type="password" />
+          </Flex>
         </Flex>
       </StyledCard>
     </Box>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,21 @@
 import { LoginCard } from "../components/LoginCard";
+import pugImage from "../assets/pug.png";
+import { Flex } from "@radix-ui/themes";
+import styled from "styled-components";
+
+const PugImage = styled.img`
+  height: 75vh;
+`;
 
 const Login = () => {
   return (
     <>
-      <LoginCard />
+      <Flex width="100vw" height="100vh" pt="15vh">
+        <LoginCard />
+      </Flex>
+      <Flex position="absolute" bottom="0" right="0">
+        <PugImage src={pugImage} alt="Cute pug" />
+      </Flex>
     </>
   );
 };
